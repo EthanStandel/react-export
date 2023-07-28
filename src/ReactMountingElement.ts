1f235c08b8b1cd6a79b20c3cb85eeaf8060ca7d5
Ethan Standel <ethan.standel@deptagency.com>
2023-07-28 18:32:38 -0400

    chore: more cleanup

--- src/ReactMountingElement.ts
+++ src/ReactMountingElement.ts
@@ -1,16 +1,12 @@
 import { createRoot } from "react-dom/client";
-import { ReactElement } from "react";
+import { createElement, type ReactElement } from "react";
 
 export abstract class ReactMountingElement extends HTMLElement {
   abstract readonly component: (props: any) => ReactElement;
   readonly #root = createRoot(this);
 
-  constructor() {
-    super();
-    this.style.display = "contents";
-  }
-
   connectedCallback() {
+    this.style.display = "contents";
     this.#mount();
   }
 
@@ -30,11 +26,10 @@ export abstract class ReactMountingElement extends HTMLElement {
   #mount() {
     this.attributes;
     const Component = this.component;
-    this.attributes.getNamedItem;
     const props = JSON.parse(
       this.attributes.getNamedItem("props")?.value ?? "{}"
     );
-    this.#root.render(<Component {...props} />);
+    this.#root.render(createElement(this.component, props));
   }
   #unmount() {
     this.#root.unmount();
