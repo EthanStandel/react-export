d6a2a6275e3001ee9b31bb42bb84944a4619d213
Ethan Standel <ethan.standel@deptagency.com>
2023-08-01 01:58:23 -0400

    chore: docs

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,17 @@ import { ReactMountingElement } from "./ReactMountingElement";
 import { ComponentProps } from "react";
 
 // Add all react components here!
+// Using a map rather than a list so that Component.name values don't get lost
+// during uglification compilation steps
 const reactElementsMap = { Counter };
+// it's important to have prefix for the component names because one of the rules
+// for naming web components is that they need to have a dash. This can be enforced
+// by having a prefix in front of the kebabCase conversion of all component names
+const PREFIX = "react";
 
 Object.entries(reactElementsMap).map(([key, component]) => {
   window.customElements.define(
-    `react-${kebabCase(key)}`,
+    `${PREFIX}-${kebabCase(key)}`,
     class extends ReactMountingElement<ComponentProps<typeof component>> {
       component = component;
     }
